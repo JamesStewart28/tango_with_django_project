09d57861e6da76e173ede7272ecb2cbeecfcb8ad
James Stewart <3009664s@student.gla.ac.uk>
2026-01-30 00:22:33 +0000

    Add index and about views with URL routing

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner!"
+                       "<br/>"
+                       '<a href="/rango/about/">About</a>'
+    )
 
 # Create your views here.
+
+def about(request):
+    return HttpResponse(
+        "Rango says here is the about page. "
+        "<br/>"
+        '<a href="/rango/">Back to Home</a>'
+    )
+                        
